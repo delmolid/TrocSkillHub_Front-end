b91ed5ddc5c2ce5b5df1bf26268d962f1a1652b2
baehl <baehl44@yahoo.fr>
2026-02-02 22:27:47 +0100

    fix: suppression du header temporaire + modif couleur en fonction de la charte graphique

--- app_front-end/src/App.tsx
+++ app_front-end/src/App.tsx
@@ -9,27 +9,11 @@ import './App.css';
 
 const App: React.FC = () => {
   // ID de l'utilisateur à afficher
-  // En production, cet ID viendrait probablement de:
-  // - React Router (useParams)
-  // - Un state global (Redux, Context)
   // - L'authentification (utilisateur connecté)
-  const userId = 1; // Affiche Jean Martin
+  const userId = 3; // Affiche Ada lovelace
 
   return (
     <div className="app">
-      {/* Header (à créer plus tard) */}
-      <header className="app-header">
-        <div className="app-header__content">
-          <h1 className="app-logo">TROC-SKILLHUB</h1>
-          <nav className="app-nav">
-            <a href="#tableau">Tableau de bord</a>
-            <a href="#offres">Offres</a>
-            <a href="#profil">Mon profil</a>
-          </nav>
-          <button className="app-disconnect">Se Déconnecter</button>
-        </div>
-      </header>
-
       {/* Contenu principal */}
       <ProfileMain userId={userId} />
 
@@ -42,7 +26,7 @@ const App: React.FC = () => {
             <a href="#faq">FAQ</a>
           </div>
           <p className="footer-copyright">
-            ©2025 Troc-SkillHub. Tous droits réservés
+            ©2026 Troc-SkillHub. Tous droits réservés
           </p>
         </div>
       </footer>
