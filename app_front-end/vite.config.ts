c958aaac99a1dda6a940e5b77147c7a6e3e5fb94
Molid <delmolid@hotmail.fr>
2026-04-17 18:09:30 +0200

    Prepare coverage step for future report and Improve workflow structure and artifacts upload

--- app_front-end/vite.config.ts
+++ app_front-end/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-  },
+test: {
+  coverage: {
+    provider: 'v8',
+    reporter: ['html'],
+    reportsDirectory: 'coverage'
+  }
+}
 })
