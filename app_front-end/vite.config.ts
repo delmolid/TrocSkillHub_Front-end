df5e080075e1ba525b99268cc50a7057cb62a5e0
Molid <delmolid@hotmail.fr>
2026-04-17 18:13:26 +0200

    ci fix setp test

--- app_front-end/vite.config.ts
+++ app_front-end/vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-test: {
-  coverage: {
-    provider: 'v8',
-    reporter: ['html'],
-    reportsDirectory: 'coverage'
-  }
-}
+  test: {
+    environment: "jsdom",
+  },
 })
