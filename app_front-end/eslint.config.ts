4ab3f64d12ec57ab7eedb0f12014f8171301bcaa
Molid <delmolid@hotmail.fr>
2026-04-20 20:27:50 +0200

    fix: add missing typescript-eslint dependencies for CI

--- app_front-end/eslint.config.ts
+++ app_front-end/eslint.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import tsParser from '@typescript-eslint/parser'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 
+
 export default defineConfig([
   globalIgnores(['dist']),
 
